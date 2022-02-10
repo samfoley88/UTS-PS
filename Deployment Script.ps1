fd593f4ec01376cea9e0aed6d49cb1b61bc40fce
Sam Foley <sam@unifiedtechnicalsolutions.co.uk>
2022-02-10 11:48:41 +0000

    feat(Deployment): Added core module with deployment function

--- Deployment Script.ps1
+++ Deployment Script.ps1
@@ -1,29 +1,27 @@
-# Download latest dotnet/codeformatter release from github
-
+Write-Information "Downloading the latest version of UTS-PS to the PowerShell module directory"
 $repo = "samfoley88/UTS-PS"
-$file = "UTS-PS.zip"
 
 $releases = "https://api.github.com/repos/$repo/releases"
 
-Write-Host Determining latest release
-$Json = Invoke-WebRequest $releases
+Write-Verbose "Getting the latest release from GitHub"
 $tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
-# https://github.com/samfoley88/UTS-PS/archive/refs/tags/v0.0.1.zip
-# $download = "https://github.com/$repo/releases/download/$tag/$file"
+Write-Information "Latest release tag is $tag"
 $download = "https://github.com/$repo/archive/refs/tags/$tag.zip"
 
 
-# Delete existing zip file
+Write-Verbose "Deleting existing zip file (if it exists)"
 Remove-Item -Path $Env:TEMP\$tag.zip -Force -ErrorAction SilentlyContinue
-Write-Host Dowloading latest release
+Write-Verbose "Dowloading latest release"
 Invoke-WebRequest $download -Out $Env:TEMP\$tag.zip
 
+Write-Verbose "Removing old zip folder (if it exists)"
 Remove-Item -Path $ENV:TEMP\UTS-PS-$tag -Force -Recurse -ErrorAction SilentlyContinue
-Write-Host Extracting release files
+Write-Verbose "Extracting zip file to temp folder"
 Expand-Archive -Path $Env:TEMP\$tag.zip -Force -DestinationPath $ENV:TEMP\UTS-PS-$tag
-# Clear out target folder
+Write-Verbose "Clearing out target folder at $env:ProgramFiles\WindowsPowerShell\Modules\UTS\*"
 Remove-Item -Path $env:ProgramFiles\WindowsPowerShell\Modules\UTS\* -Force -Recurse -ErrorAction SilentlyContinue
 # Remove v from tag because its not there in folder
 $versionNumber = $tag.replace('v','')
+Write-Verbose "Copying module to target folder"
 Copy-Item -Path "$ENV:TEMP\UTS-PS-$tag\UTS-PS-$versionNumber\*" -Destination $env:ProgramFiles\WindowsPowerShell\Modules\UTS -Recurse
 
---
+++ UTS.Core/UTS.Core.psm1
@@ -0,0 +1,76 @@
+function Get-UTSInstalled {
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    return $True
+}
+
+function Update-UTSPS {
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    #region Define variables
+    $UTSModuleFolder = "$env:ProgramFiles\WindowsPowerShell\Modules\UTS"
+    #endregion Define variables
+
+    #region Check Access
+    Write-Verbose "Checking for write access to modules folder"
+    try {
+            Out-File -FilePath "$UTSModuleFolder\WritePermissionsTest.txt"
+    }
+    catch {
+        Write-Error "No write access to modules folder"
+        return
+    }
+    #endregion Check Access
+
+    #region Download and unzip release
+    Write-Information "Downloading the latest version of UTS-PS to the PowerShell module directory"
+    $repo = "samfoley88/UTS-PS"
+
+    $releases = "https://api.github.com/repos/$repo/releases"
+
+    Write-Verbose "Getting the latest release from GitHub"
+    $tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+    Write-Information "Latest release tag is $tag"
+    $download = "https://github.com/$repo/archive/refs/tags/$tag.zip"
+
+
+    Write-Verbose "Deleting existing zip file (if it exists)"
+    Remove-Item -Path $Env:TEMP\$tag.zip -Force -ErrorAction SilentlyContinue
+    Write-Verbose "Dowloading latest release"
+    Invoke-WebRequest $download -Out $Env:TEMP\$tag.zip
+
+    Write-Verbose "Removing old zip folder (if it exists)"
+    Remove-Item -Path $ENV:TEMP\UTS-PS-$tag -Force -Recurse -ErrorAction SilentlyContinue
+    Write-Verbose "Extracting zip file to temp folder"
+    Expand-Archive -Path $Env:TEMP\$tag.zip -Force -DestinationPath $ENV:TEMP\UTS-PS-$tag
+    #endregion Download and unzip release
+    
+    #region Copy files to module folder
+    Write-Verbose "Clearing out target folder at $UTSModuleFolder\*"
+    Remove-Item -Path $UTSModuleFolder\* -Force -Recurse -ErrorAction SilentlyContinue
+    # Remove v from tag because its not there in folder
+    $versionNumber = $tag.replace('v','')
+    Write-Verbose "Copying module to target folder"
+    Copy-Item -Path "$ENV:TEMP\UTS-PS-$tag\UTS-PS-$versionNumber\*" -Destination $UTSModuleFolder\* -Recurse
+    #endregion Copy files to module folder
+
+    #region Update version number
+    Write-Verbose "Updating version number in module manifests"
+    Get-ChildItem $UTSModuleFolder -Filter *.psd1 -Recurse | Format-Table
+    Get-ChildItem $UTSModuleFolder -Filter *.psd1 -Recurse | 
+        Foreach-Object {
+             $OriginalManifest = get-content -Path $_.FullName -Raw
+             $UpdatedManifest = $OriginalManifest -replace "0.0.0",$versionNumber
+             Write-Host $UpdatedManifest
+             Set-Content -Path $_.FullName -Value $UpdatedManifest
+        }
+    
+    #endregion Update version number
+}
+
