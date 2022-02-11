a299060adfe3eb2a8e48a363eb8525a7e1379c17
Sam Foley <sam@unifiedtechnicalsolutions.co.uk>
2022-02-11 11:28:14 +0000

    feat(UTS.Monitoring): Added Get-UTSConnectivity module
    Performs ping and provides simple up/down response with single target

---
+++ UTS.Monitoring/UTS.Monitoring.psm1
@@ -0,0 +1,34 @@
+function Get-UTSConnectivity {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)][string]$Target
+    )
+    
+    $InitialStatus = Test-Connection $Target -Count 1 -Quiet
+    if ($InitialStatus -eq $True) {
+        Write-Output "$(Get-Date -UFormat "%Y-%m-%d | %T") |     $Target is currently online"
+    } else {
+        Write-Output "$(Get-Date -UFormat "%Y-%m-%d %T") $Target is currently offline"
+    }
+
+    $CurrentStatus = $InitialStatus
+
+    While ($True) {
+        $NewStatus = Test-Connection $Target -Count 1 -Quiet
+        Write-Debug "$(Get-Date -UFormat "%Y-%m-%d %T") Debug: Current status: $Target is $NewStatus"
+
+        if ($NewStatus -eq $CurrentStatus) {
+            # Do nothing as status hasn't changed
+        } else {
+            if ($NewStatus -eq $True) {
+                Write-Output "$(Get-Date -UFormat "%Y-%m-%d %T") $Target is now online"
+            } else {
+                Write-Output "$(Get-Date -UFormat "%Y-%m-%d %T") $Target is now offline"
+            }
+        }
+
+        $CurrentStatus = $NewStatus
+        Start-Sleep -Milliseconds 250
+    }
+   
+}
