65008ec67487df6d9f92752989f5355cea51721a
Sam Foley <sam@unifiedtechnicalsolutions.co.uk>
2022-02-18 11:41:54 +0000

    feat(Get-UTSUnprocessed): Checks if we have seen this event/item before
    This function takes an array of object and compares a provided unique identifier in that object to a history file to see if it has been seen before, it returns any that haven't been seen before.

--- UTS.Core/UTS.Core.psm1
+++ UTS.Core/UTS.Core.psm1
@@ -1,3 +1,18 @@
+#region Create Module Variable Hashtable
+$UTSCoreVariables = [ordered]@{
+    UTSDataStorePath           = "$($env:ProgramFiles)\UTS\DataStore";
+    ClientUsername      = 'username'
+}
+New-Variable -Name UTSCoreVariables  -Value $UTSCoreVariables -Scope Script -Force
+#endregion Create Module Variable Hashtable
+
+#region Create required elements for core functionality
+New-Item -ItemType Directory -Force -Path $UTSCoreVariables.UTSDatastorePath | Out-Null
+
+#region Create required elements for core functionality
+
+
+
 function Get-UTSInstalled {
     [CmdletBinding()]
     param (
@@ -83,3 +98,114 @@ function Update-UTSPS {
     #endregion Update version number
 }
 
+# 
+function Get-UTSUnprocessed {
+    <#
+    .SYNOPSIS
+        Deduplicates the provided list of objects against a file stored in json
+    .OUTPUTS
+        Returns a collection of objects that are not duplicates
+    .EXAMPLE
+        $TestArray = Get-ChildItem -Path C:\
+        Get-UTSUnprocessed -Array $TestArray -FilteringProperty Name -UniqueHistoryId "Test"
+        
+    #>
+
+    [CmdletBinding()]
+    param (
+        # Our array of objects to deduplicate
+        [Parameter(Mandatory=$True)]
+        [array]
+        $Array,
+        # The property in the input object to use to deduplicate
+        [Parameter(Mandatory=$True)]
+        [string]
+        $FilteringProperty,
+        # A unique identifier for this deduplication store, must be unique across this machine
+        [Parameter()]
+        [string]
+        $UniqueHistoryId,
+        # An optional path to a non-standard history file location
+        [Parameter()]
+        [string]
+        $HistoryFile
+    )
+
+    #region Define variables
+    if ($HistoryFile -eq "") {
+        $HistoryFile = "$($UTSCoreVariables.UTSDatastorePath)\ProcessingHistory\$UniqueHistoryId.json"
+    } elseif (($null -eq $HistoryFile) -and ($UniqueHistoryId -eq $null)) {
+        Write-Error "Must provide either a history file or a unique history id"
+        exit 2
+    }
+    #endregion Define variables
+
+    #region Check if history file exists
+    Write-Debug "Checking if history file exists"
+    if (Test-Path -Path $HistoryFile) {
+        Write-Debug "History file exists"
+        #region Get history
+        Write-Debug "Getting history"
+        $HistoryFileRaw = Get-Content -Path $HistoryFile -Raw
+        Write-Debug "History raw file is: [$HistoryFileRaw]"
+        if ($HistoryFileRaw -eq $null) {
+            Write-Debug "History file is empty"
+            $History = New-Object System.Collections.Generic.List[Hashtable]
+        } else {
+            Write-Debug "History file is not empty"
+            $History = ConvertFrom-Json $HistoryFileRaw
+            # Remove unwanted object nesting if exists
+            if ($null -ne $History.value){
+                $History = $History.value
+            }
+        }
+        #endregion Get history
+    } else {
+        Write-Debug "History file does not exist"
+        Write-Information "Creating history file"
+        New-Item -Path $HistoryFile -ItemType File -Force
+        Write-Information "Creating blank history property"
+        $History =  New-Object System.Collections.Generic.List[Hashtable]
+    }
+    #endregion Check if history file exists
+
+    
+
+    #region Compare new to old, remove old from new array and add new to old history
+    # Process each item in new array and see if its FilterProperty is in the history
+    # Process each item in new array and see if its FilterProperty is in the history
+    $NewItems = New-Object System.Collections.Generic.List[Hashtable]
+    
+    Write-Debug "Comparing new array to history"
+    foreach ($item in $Array) {
+        Write-Debug "Checking if [$item.$FilteringProperty] is in history"
+        if ($History -contains $item.$FilteringProperty) {
+            Write-Debug "Found [$item.$FilteringProperty] in history"
+        } else {
+            Write-Debug "Did not find [$item.$FilteringProperty] in history"
+            Write-Debug "Adding [$item.$FilteringProperty] to history"
+            $History += $item.$FilteringProperty
+            Write-Debug "Adding item to return array"
+            $NewItems += $item
+        }
+    }
+    #endregion Compare new to old, remove old from new array and add new to old history
+
+    #endregion Compare new to old, remove old from new array and add new to old history
+    
+
+
+
+    #region Update history
+    Write-Debug "Updating history"
+    $UpdatedHistoryJson = ConvertTo-Json $History -Compress
+    Write-Debug "Updated history is: [$UpdatedHistoryJson]"
+    Write-Information "Writing history to file"
+    Set-Content -Path $HistoryFile -Value $UpdatedHistoryJson
+    #endregion Update history
+
+    #region Return deduplicated array
+    return $NewItems
+    #endregion Return deduplicated array
+
+}
