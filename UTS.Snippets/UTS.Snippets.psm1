4693e89b406a09ff43cc626dedaeb790bab36a01
Sam Foley <sam@unifiedtechnicalsolutions.co.uk>
2022-02-18 14:20:58 +0000

    feat(Get-RandomPassword): Added random password generator
    Simplistic and character driven but will work for now

--- UTS.Snippets/UTS.Snippets.psm1
+++ UTS.Snippets/UTS.Snippets.psm1
@@ -1,3 +1,13 @@
 function Get-UTSAllHistory {
     Get-Content (Get-PSReadlineOption).HistorySavePath
 }
+
+function Get-RandomPassword {
+    param (
+        [Parameter()]
+        [int] $length = 40,
+        [int] $amountOfNonAlphanumeric = 1
+    )
+    Add-Type -AssemblyName 'System.Web'
+    return [System.Web.Security.Membership]::GeneratePassword($length, $amountOfNonAlphanumeric)
+}
