d94faa38bb4bab266652cbd5133e628821653375
Sam Foley <sam@unifiedtechnicalsolutions.co.uk>
2022-02-25 16:12:00 +0000

    feat(Get-UTSVariablesFromJson): Added to convert json to variables

--- UTS.Snippets/UTS.Snippets.psm1
+++ UTS.Snippets/UTS.Snippets.psm1
@@ -11,3 +11,25 @@ function Get-UTSRandomPassword {
     Add-Type -AssemblyName 'System.Web'
     return [System.Web.Security.Membership]::GeneratePassword($length, $amountOfNonAlphanumeric)
 }
+
+# Add documentation comment
+function Get-UTSVariablesFromJson {
+    <#
+    .SYNOPSIS
+        Converts a JSON string (single level) to individual variables.
+    .OUTPUTS
+        None itself, the variables are set in the script scope
+    .EXAMPLE
+        Get-UTSVariablesFromJson -Json $JsonString
+        
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        # The JSON string to parse
+        [string] $Json
+    )
+    
+    $Json | ConvertFrom-Json | ForEach {$_.PSObject.Properties | ForEach-Object { New-Variable -Name $_.Name -Value $_.Value -Force -Scope global}}
+
+}
