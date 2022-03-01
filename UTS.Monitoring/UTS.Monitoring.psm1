7e7d5ed61cb56e9c5f42e9599a451263cb4cb536
Sam Foley <sam@unifiedtechnicalsolutions.co.uk>
2022-03-01 14:01:20 +0000

    feat(Test-UTSDomainConnectivity): Added to check basic domain connection

--- UTS.Monitoring/UTS.Monitoring.psm1
+++ UTS.Monitoring/UTS.Monitoring.psm1
@@ -35,3 +35,110 @@ function Get-UTSConnectivity {
     }
    
 }
+
+
+function Test-UTSDomainConnectivity {
+    <#
+    .SYNOPSIS
+        Test connectivity to the domain stepping through each possible issue type Basic DNS -> SRV -> Host resolution -> Trust test
+    .OUTPUTS
+        Write errors to Warning, and info and verbose to those respectively
+    .EXAMPLE
+        # To view the output of this function, use the following command
+        $InformationPreference = $VerbosePreference = "Continue"
+        Test-UTSDomainConnectivity
+        
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    #region Define variables
+    $Warning = [System.Collections.Generic.List[string]]::new()
+    #endregion Definte variables
+
+    #region Check all DNS Servers are private
+    Write-Information "Checking DNS Servers are all private IPs"
+    $DNSServers = Get-DnsClientServerAddress -AddressFamily IPv4 | Select-Object -ExpandProperty ServerAddresses -Unique
+    Write-Verbose "DNS Servers are [$DNSServers]"
+    Write-Verbose "Checking if those are private or public"
+    $DNSServers | ForEach-Object {
+        if (Test-UTSPrivateIP -IP $_) {
+            Write-Verbose "$_ is a private IP"
+        } else {
+            $NewWarning = "$_ is a public IP"
+            Write-Verbose $NewWarning
+            $Warning = $Warning + $NewWarning
+        }
+    }
+    Write-Information "Finished DNS Private IP Checks."
+
+    #endregion Check all DNS Servers are private
+
+    #region Check all DNS servers resolve the domain kerberos and ldap records
+    Write-Information "Testing we can resolve domain controllers"
+    $Domain = (Get-WmiObject Win32_ComputerSystem).Domain
+    Write-Verbose "Domain is [$Domain]"
+    Write-Verbose "Getting LDAP records"
+
+    $LDAPRecords = Resolve-DnsName -Name "_ldap._tcp.dc._msdcs.$Domain" -Type SRV -ErrorAction "SilentlyContinue"
+    if ($null -eq $LDAPRecords) {
+        $NewWarning = "No LDAP Records found for domain [$Domain]"
+        Write-Verbose $NewWarning
+        $Warning = $Warning + $NewWarning
+    } else {
+        Write-Verbose "LDAP records found"
+        $LDAPRecords | Where-Object {$null -ne $_.IP4Address} | ForEach-Object { 
+            Test-Connection $_.IP4Address -Count 1 | Out-Null
+        }
+    }
+    
+    Write-Verbose "LDAP test completed"
+    
+    Write-Verbose "Getting kerberos records"
+
+    $kerberosRecords = Resolve-DnsName -Name "_kerberos._tcp.dc._msdcs.$Domain" -Type SRV -ErrorAction "SilentlyContinue"
+    if ($null -eq $kerberosRecords) {
+        $NewWarning = "No kerberos Records found for domain [$Domain]"
+        Write-Verbose $NewWarning
+        $Warning = $Warning + $NewWarning
+    } else {
+        Write-Verbose "kerberos records found"
+        $kerberosRecords | Where-Object {$null -ne $_.IP4Address} | ForEach-Object { 
+            Test-Connection $_.IP4Address -Count 1 | Out-Null
+        }
+    }
+    
+    Write-Verbose "kerberos test completed"
+    
+    
+    #endregion Check all DNS servers resolve the domain kerberos and kerberos records
+
+    Write-Information "Finished testing domain controllers."
+
+    Write-Information "Running final domain connection check"
+    if ((Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain){
+        $SecureChannel = Test-ComputerSecureChannel -ErrorAction "SilentlyContinue"
+        if ($SecureChannel -eq $false) {
+            
+            $NewWarning = "Test-ComputerSecureChannel failed"
+            Write-Verbose $NewWarning
+            $Warning = $Warning + $NewWarning
+        }
+        Write-Information "Final check finished."
+    } else {
+        Write-Information "Computer is not part of a domain, not running final check"
+    }
+        
+    Write-Information "All checks are finished."
+
+    if ($null -ne $Warning) {
+        Write-Information "The following warnings were raised:"
+        $WarningString = $Warning -Join "`n"
+        Write-Information $WarningString
+        return $False
+    } else {
+        Write-Information "No warnings were raised, returning true"
+        return $True
+    }
+}
